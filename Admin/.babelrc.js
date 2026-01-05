f4b46aefaa315d65a6d3fd08558f7103a6976028
Axit Vora <axitvora820@gmail.com>
2026-01-05 10:15:54 +0530

    Update .babelrc.js

--- Admin/.babelrc.js
+++ Admin/.babelrc.js
@@ -1,14 +1,1 @@
-module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        loose: true,
-        bugfixes: true,
-        modules: false,
-        exclude: ['transform-typeof-symbol']
-      }
-    ]
-  ]
-};
 
