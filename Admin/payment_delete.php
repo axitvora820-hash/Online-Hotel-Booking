5ae04cc08b3036633b3861e0edf54f1a3a7c6756
Axit Vora <axitvora820@gmail.com>
2026-01-18 09:37:54 +0530

    Update payment_delete.php

--- Admin/payment_delete.php
+++ Admin/payment_delete.php
@@ -1,1 +1,13 @@
+<?php
 
+include '../connection.php';
+
+$id = $_GET['id'];
+
+$deletesql = "DELETE FROM payment WHERE id = $id";
+
+$result = mysqli_query($conn, $deletesql);
+
+header("Location:payment.php");
+
+?>
